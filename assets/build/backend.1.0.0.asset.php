2a3f2fada20b19e1ac3cc537d953311b733fe2e5
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2025-11-25 08:59:46 +0600

    build push

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => 'fddaa5d1c16bb13a63e6');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => 'd5a99ee462587d6c36f5');
