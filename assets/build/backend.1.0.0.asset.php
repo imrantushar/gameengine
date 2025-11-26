28d405dd45e65c22bf409808b90c6216b78e6fdb
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2025-11-26 10:49:08 +0600

    Add checkbox to Name column in Points table
    Introduced a checkbox input to each row in the Name column for selection purposes and disabled pagination in the Points table. Checkbox state logs the selected row's id and checked status to the console.

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '8723ff63dc8667cb7e83');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '7bdee176da4a641ce830');
