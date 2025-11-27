cc5df1a7ed274f6e9bdeb6e2e986bbfe2ee385c1
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2025-11-27 08:02:48 +0600

    after marge build

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '7a40931169f761c8d3bf');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '7f617e6ef9f1b1d6cfec');
