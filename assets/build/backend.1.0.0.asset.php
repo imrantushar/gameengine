b89a957ccd3bb917b0733ace439c1ad586618257
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2025-12-02 11:24:08 +0600

    modal tricger update

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => 'bb802a45e5f385b83795');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '980c35a5d151a39778ee');
