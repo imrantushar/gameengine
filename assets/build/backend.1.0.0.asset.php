32a6fbf50e42f749114648b08965726f3e7833c8
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2025-12-03 20:36:43 +0600

    react midle ware add

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '865917f5964b59000004');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '45438463db21c8a78ab5');
