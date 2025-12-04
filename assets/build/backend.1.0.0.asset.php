1ef09b8eb9b0dee4f42936108953f4e7d10299a2
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2025-12-04 19:13:04 +0600

    icon file add but need to support icomoon its just demo add

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ddf26a34c46d16e8d285');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3586834751e87e88e014');
