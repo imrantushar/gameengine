24a755fae953b32bfe345d085fac7e247ea5e4a7
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2025-12-07 08:23:15 +0600

    user navigator issue solved

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3586834751e87e88e014');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'fb9e3fdf85be647c530b');
