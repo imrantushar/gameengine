99b1f904287e27e79c71f078f6474767d4de1bb2
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2025-12-18 20:06:06 +0600

    solved tab issues

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4659f3aedcda8106b730');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '25676085e1651b9cacab');
