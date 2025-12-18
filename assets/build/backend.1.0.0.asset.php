c604318fb1233a24ea0f612cdc68746607cb43be
Raihan Islam <raihanislam.cse@gmail.com>
2025-12-18 18:36:50 +0600

    updat

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '04938271b34e776dc8c8');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5d52f933c5e6cebf2631');
