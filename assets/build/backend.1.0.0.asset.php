c4b3d3254d0ba8ed7db44f278ff7e14cfb7bdb11
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-01-04 16:56:10 +0600

    decodeded add

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '5c449974b51bab2d71f4');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '3c67efdb496a2522ab93');
