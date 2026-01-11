4d060e8da6983b225d7a4c80ffed2036892872ce
mizanurrahman70 <mizanurrahmanofficial70@gmail.com>
2026-01-11 09:18:15 +0600

    levlei secleted issues solved

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '88422862962a9429687d');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '58287947e1d6d3b4de62');
