de3ef2cdffe78715b63055d51b9583ce057b79cd
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-01-12 14:06:37 +0600

    code cleaned and skeleton loader added

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'aaff15af7f3e0a681a54');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'a794e6fcd8717d48c74d');
