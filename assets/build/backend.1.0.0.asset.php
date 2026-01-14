923670b51ae757e8b9ff41019adc94cf62d60d54
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-01-14 17:39:41 +0600

    get help markup and functionality added

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'a214e57dc3a8769da103');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'fdd65a57b767861c293d');
