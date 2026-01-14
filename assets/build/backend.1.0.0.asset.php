03b67c8117838ad5702bb94a06b7524a3f216c3f
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-01-14 16:30:40 +0600

    formik applied for settings

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'a4356a8433e8e88f4b67');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'baa61a59a2d26af494e5');
