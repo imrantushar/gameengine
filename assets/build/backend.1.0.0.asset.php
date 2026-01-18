5e645d886c6f1dad1c53c36eae37e322757464a5
Raihan Islam <raihanislam.cse@gmail.com>
2026-01-18 13:53:57 +0600

    update installer

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'fd4f412c3647d2dca3e0');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'a86d7ae07beff590814c');
