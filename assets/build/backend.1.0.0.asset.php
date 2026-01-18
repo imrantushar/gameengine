65d734b35a0dd3f6e1690e615b38d45cff91264c
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-01-18 16:45:19 +0600

    notification render issue fixed

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'a86d7ae07beff590814c');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '98a08623dc51ccf3dea3');
