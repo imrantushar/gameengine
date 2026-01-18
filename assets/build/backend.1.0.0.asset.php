27ed4e2d69736f39538ecbd07b635bcdbb5c1739
Raihan Islam <raihanislam.cse@gmail.com>
2026-01-18 15:43:20 +0600

    update logs code

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '7eb266acb5e77f13248f');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '1e64616afc2a2558f24d');
