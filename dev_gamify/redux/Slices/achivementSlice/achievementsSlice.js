9b297cf0af4fbf3eb922078b8db2729c67012f1c
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-01-19 14:04:28 +0600

    fetch all item and delete item action updated

--- dev_gamify/redux/Slices/achivementSlice/achievementsSlice.js
+++ dev_gamify/redux/Slices/achivementSlice/achievementsSlice.js
@@ -1,30 +1,61 @@
+import { API, namespace } from '@GFUtils/helper';
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import apiFetch from '@wordpress/api-fetch';
+import { showNotification } from '../notificationSlice/notificationSlice';
+import { __ } from '@wordpress/i18n';
 
 // --- Async Thunks ---
-export const fetchAchievements = createAsyncThunk('achievements/fetchAll', async () => {
-    return await apiFetch({ path: '/gamify/v1/achievements' });
+export const fetchAchievements = createAsyncThunk('gamify/fetchAchievements', async () => {
+    const response =  await API.get(namespace + 'achievements');
+
+    console.log({fetchAchievements: response})
+    return response.data;
 });
 
-export const fetchAchievementById = createAsyncThunk('achievements/fetchById', async (id) => {
-    return await apiFetch({ path: `/gamify/v1/achievements/${id}` });
+export const fetchAchievementById = createAsyncThunk('gamify/fetchAchievementById', async (id) => {
+    const response =  await API.get(namespace + 'achievements/' + id);
+    console.log({response})
+    return response.data;
 });
 
-export const saveAchievement = createAsyncThunk('achievements/save', async (data) => {
-    return await apiFetch({ path: '/gamify/v1/achievements', method: 'POST', data });
+export const saveAchievement = createAsyncThunk('gamify/saveAchievement', async (data) => {
+    // return await apiFetch({ path: '/gamify/v1/achievements', method: 'POST', data });
+    const response =  await API.post(namespace + 'achievements', {
+        ...data
+    });
+    console.log({saveAchievement: response})
+    return response.data;
 });
 
-export const updateAchievement = createAsyncThunk('achievements/update', async ({ id, data }) => {
-    return await apiFetch({ path: `/gamify/v1/achievements/${id}`, method: 'PUT', data });
+export const updateAchievement = createAsyncThunk('gamify/updateAchievement', async ({ id, data }) => {
+    // return await apiFetch({ path: `/gamify/v1/achievements/${id}`, method: 'PUT', data });
+    const response =  await API.post(namespace + 'achievements/' + id, {
+        ...data
+    });
+    console.log({updateAchievement: response})
+    return response.data;
 });
 
-export const deleteAchievement = createAsyncThunk('achievements/delete', async (id) => {
-    await apiFetch({ path: `/gamify/v1/achievements/${id}`, method: 'DELETE' });
+export const deleteAchievement = createAsyncThunk('gamify/deleteAchievement', async (id, {dispatch}) => {
+    await API.post(namespace + 'achievements/' + id,
+        { force: false },
+        {
+            headers: {
+                'X-HTTP-Method-Override': 'DELETE',
+            },
+        }
+    );
+
+    dispatch(showNotification({
+        message: __('Achivement deleted successfully.', 'gamify'),
+        isShow: true,
+        type: 'success',
+    }))
+
     return id;
 });
 
-export const fetchTriggers = createAsyncThunk(
-    'achievements/fetchTriggers',
+export const fetchTriggers = createAsyncThunk('gamify/fetchTriggers',
     async (scope = 'achievement', { rejectWithValue }) => {
         try {
             return await apiFetch({ path: `/gamify/v1/triggers?scope=${scope}` });
@@ -34,8 +65,7 @@ export const fetchTriggers = createAsyncThunk(
     }
 );
 
-export const fetchDynamicOptions = createAsyncThunk(
-    'achievements/fetchDynamicOptions',
+export const fetchDynamicOptions = createAsyncThunk('gamify/fetchDynamicOptions',
     async ({ integration, query }, { rejectWithValue }) => {
         try {
             return await apiFetch({ path: '/gamify/v1/dynamic', method: 'POST', data: { integration, query } });
@@ -45,12 +75,14 @@ export const fetchDynamicOptions = createAsyncThunk(
     }
 );
 
-export const fetchPointTypes = createAsyncThunk('achievements/fetchPointTypes', async () => {
-    return await apiFetch({ path: '/gamify/v1/point-types' });
+export const fetchPointTypes = createAsyncThunk('gamify/fetchPointTypes', async () => {
+    const response =  await API.get(namespace + 'point-types');
+    return response.data;
 });
 
 const initialState = {
     achievements: [],
+    availablePointTypes: [],
     integrations: {},
     allHooks: [],
     hookSettings: {},
@@ -102,8 +134,11 @@ const achievementsSlice = createSlice({
                     });
                 }
             })
-            .addCase(saveAchievement.fulfilled, (state) => { state.saveStatus = 'saved'; })
-            .addCase(updateAchievement.fulfilled, (state) => { state.saveStatus = 'saved'; });
+            // .addCase(saveAchievement.fulfilled, (state) => { state.saveStatus = 'saved'; })
+            // .addCase(updateAchievement.fulfilled, (state) => { state.saveStatus = 'saved'; });
+            .addCase(deleteAchievement.fulfilled, (state, {payload}) => { 
+                state.achievements = state.achievements.filter(item => Number(item.id) !== Number(payload)) 
+            })
     }
 });
 
