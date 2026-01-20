06054938325ae935839df331736e4758e9bf23d9
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-01-20 09:00:13 +0600

    build push

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'f0f567b2d7c41fca7c7f');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '220a0f8a63bb9c4e2120');
