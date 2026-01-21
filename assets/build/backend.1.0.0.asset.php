ae2fdb2be27336fc4012cb1eacc8988c0e4a8199
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-01-21 14:25:55 +0600

    table loader fixed

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'ea2219c368d61ec968d8');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '8f688f60699c794340c3');
--- dev_gamify/containers/BackendDashboard/pages/logs/LogsTable.js
+++ dev_gamify/containers/BackendDashboard/pages/logs/LogsTable.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { __ } from '@wordpress/i18n';
 import GFLabel from '@GFComponents/Labels/GFLabel';
@@ -18,17 +18,22 @@ const LogsTable = ({ modalOpenHandler }) => {
         search,
         status
     } = useSelector((state) => state.logs);
+    const [loading, setLoading] = useState(items.length === 0);
 
 
-    const handleRefresh = (page = 1, per_page = 10, serchKey = "") => {
-        dispatch(fetchLogs({ page, per_page, search: serchKey }));
+    const handleRefresh = async (page = 1, per_page = 10, serchKey = "") => {
+        setLoading(true)
+        await dispatch(fetchLogs({ page, per_page, search: serchKey }));
+        setLoading(false)
     };
 
     useEffect(() => {
         handleRefresh()
     }, []);
 
-    const handlePageChange = (newPage) => handleRefresh(newPage, perPage);
+    const handlePageChange = (newPage) => {
+        handleRefresh(newPage, perPage)
+    };
 
     const handlePerPageChange = (itemsPerPage) => {
         handleRefresh(currentPage, itemsPerPage)
@@ -163,7 +168,7 @@ const LogsTable = ({ modalOpenHandler }) => {
                         fontWeight="400"
                         lineHeight="16px"
                         borderRadius="4px"
-                        onClick={handleRefresh}
+                        onClick={() => handleRefresh()}
                     >
                         {status === "loading" ? "loading..." : "refresh"}
                     </Button>
@@ -198,7 +203,7 @@ const LogsTable = ({ modalOpenHandler }) => {
                 perPage={perPage}
                 onChangePage={handlePageChange}
                 onChangeItemsPerPage={handlePerPageChange}
-                dataFetchingStatus={status === 'loading'}
+                dataFetchingStatus={loading}
                 suffix="logs-table"
             />
         </>
