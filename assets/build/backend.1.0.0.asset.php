4e31b95bcd438104b80cd1de13592a677590223e
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-01-21 14:53:53 +0600

    Field label updated

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '3d4158223c82b5fe133b');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '6f715141a952d47395c9');
