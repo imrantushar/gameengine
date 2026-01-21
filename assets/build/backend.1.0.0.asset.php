1acb5009c7fc82049ec2b687e6186b2e982d244c
Raihan Islam <raihanislam.cse@gmail.com>
2026-01-21 13:47:25 +0600

    build

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '689be1314eb2f7e9e544');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'f3fd20036eb1add7ede2');
