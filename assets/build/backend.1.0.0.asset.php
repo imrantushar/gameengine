2c04cd420b80b6ffa79a544eb7cdc957892e3268
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-01-24 22:59:02 +0600

    Settings added

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'c415793bbc132343fce9');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '921126de344a32bd7e77');
--- dev_gamify/containers/BackendDashboard/pages/settings/Tabs/GeneralSettings.js
+++ dev_gamify/containers/BackendDashboard/pages/settings/Tabs/GeneralSettings.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Box, Flex, Input, Text, VStack } from '@chakra-ui/react';
+import { Box, Checkbox, Flex, Input, Separator, Text, VStack } from '@chakra-ui/react';
 import { __ } from "@wordpress/i18n";
 import GFLabel from '@GFComponents/Labels/GFLabel';
 import LabeledInput from '@GFComponents/LabeledInput';
@@ -8,7 +8,38 @@ import SettingsInner from '../Components/SettingsInner';
 import { useDispatch } from 'react-redux';
 import { commonInput } from '../../../../../../assets/scss/chakra/recipe';
 import GamifyInput from '@GFComponents/GamifyInput';
+import Select from "react-select";
 import { useFormikContext } from 'formik';
+
+const displaycicle = [
+    {
+        label: __('Immediate', 'gamify'),
+        value: 'immediate',
+    },
+    {
+        label: __('After 24 Hours', 'gamify'),
+        value: 'daily',
+    },
+    {
+        label: __('After 7 Days', 'gamify'),
+        value: 'weekly',
+    },
+]
+
+const retentionDays = [
+    {
+        label: __('Never', 'gamify'),
+        value: '0',
+    },
+    {
+        label: __('30 Days', 'gamify'),
+        value: '30',
+    },
+    {
+        label: __('90 Days', 'gamify'),
+        value: '90',
+    },
+]
 
 const GeneralSettings = () => {
     const {values, setFieldValue} = useFormikContext();
@@ -19,29 +50,98 @@ const GeneralSettings = () => {
             <Flex direction="column" gap='16px'>
                 <GFLabel type="title" label={__("Level Image Size", "gamify")} />
 
-                <Flex gap='12px'>
-                    <GamifyInput label={__("Max Width", "gamify")}>
-                        <Input
-                            placeholder={__("Enter max width", "gamify")}
-                            type="number"
-                            value={general?.level_image_width}
-                            onChange={(e) => setFieldValue('general.level_image_width', e.target.value )}
-                            {...commonInput}
-                        />
-                    </GamifyInput>
+                <GamifyInput 
+                    label={__("Log Display", "gamify")} 
+                    width="100%" 
+                    direction={'row'}
+                    justifyContent="space-between"
+                >
+                    <Select
+                        className="gamify-select gamify-select--300"
+                        classNamePrefix="gamify-select"
+                        options={displaycicle}
+                        value={
+                            displaycicle?.find(
+                            opt => opt.value === values?.display_cycle
+                            ) || null
+                        }
+                        onChange={option => {
+                            setFieldValue('display_cycle', option.value)
+                        }}
+                        menuPlacement="bottom"
+                    />
+                </GamifyInput>
 
-                    <GamifyInput label={__("Max Height", "gamify")}>
-                        <Input
-                            placeholder={__("Enter max height", "gamify")}
-                            type="number"
-                            value={general?.level_image_height}
-                            onChange={(e) => setFieldValue('general.level_image_height', e.target.value )}
-                            {...commonInput}
-                        />
-                    </GamifyInput>
-                </Flex>
+                <GamifyInput 
+                    label={__("Auto Cleanup", "gamify")}
+                    width="100%" 
+                    direction={'row'}
+                    justifyContent="space-between"
+                >
+                    <Select
+                        className="gamify-select gamify-select--300"
+                        classNamePrefix="gamify-select"
+                        options={retentionDays}
+                        value={
+                            retentionDays?.find(
+                            opt => Number(opt.value) === Number(values?.retention_days)
+                            ) || null
+                        }
+                        onChange={option => {
+                            setFieldValue('retention_days', option.value)
+                        }}
+                        menuPlacement="bottom"
+                    />
+                </GamifyInput>
 
-                <GFLabel type="subtitle" label={__("Maximum dimensions for ranks featured image.", "gamify")} />
+                <GFLabel type="title" label={__("Log Levels", "gamify")} />
+                <Separator />
+                <GamifyInput 
+                    label={__("Enable successful rewards", "gamify")}
+                    width="100%" 
+                    direction={'row'}
+                    justifyContent="space-between"
+                >
+                    <Checkbox.Root
+                        size="sm"
+                        mt="0.5"
+                        aria-label="Select row"
+                        checked={values?.log_levels?.includes('success')}
+                        onCheckedChange={(changes) => {
+                            if(changes.checked) {
+                                setFieldValue('log_levels', values.log_levels.filter(item => item !== 'success'))
+                            } else {
+                                setFieldValue('log_levels', [...values.log_levels, 'success'])
+                            }
+                        }}
+                    >
+                        <Checkbox.HiddenInput />
+                        <Checkbox.Control />
+                    </Checkbox.Root>
+                </GamifyInput>
+                <GamifyInput 
+                    label={__("Enable System Errors", "gamify")}
+                    width="100%" 
+                    direction={'row'}
+                    justifyContent="space-between"
+                >
+                    <Checkbox.Root
+                        size="sm"
+                        mt="0.5"
+                        aria-label="Select row"
+                        checked={values?.log_levels?.includes('error')}
+                        onCheckedChange={(changes) => {
+                            if(changes.checked) {
+                                setFieldValue('log_levels', values.log_levels.filter(item => item !== 'error'))
+                            } else {
+                                setFieldValue('log_levels', [...values.log_levels, 'error'])
+                            }
+                        }}
+                    >
+                        <Checkbox.HiddenInput />
+                        <Checkbox.Control />
+                    </Checkbox.Root>
+                </GamifyInput>
             </Flex>
         </SettingsInner>
     );
