4dc0b61c6d4746b563f9a2130a1572b36299be60
Raihan Islam <raihanislam.cse@gmail.com>
2026-01-25 11:50:52 +0600

    update

--- includes/assets.php
+++ includes/assets.php
@@ -73,7 +73,8 @@ class Assets
             'route_path'         => wp_parse_url(admin_url(), PHP_URL_PATH),
             'menu'               => wp_json_encode(Helper::get_admin_menu_list()),
             'is_plain_permalink' => (bool) empty(get_option('permalink_structure')),
-            'is_pro' => file_exists(GAMIFY_PATH . 'includes/pro/init.php'),
+            //'is_pro' => file_exists(GAMIFY_PATH . 'includes/pro/init.php'),
+            'is_pro' => false
         );
     }
 
