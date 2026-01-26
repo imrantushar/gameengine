aa4e7035d8c50d04d78df8d6598949b0881cb659
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-01-26 10:07:54 +0600

    types parent shown on tabel

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '6016fee8869ef90b97cc');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '09b96b7dcd832f124d48');
