5bbddaa61af7fb34fadc002794415a403c522aa4
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-01-26 11:37:13 +0600

    minor ui adjustment

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '09b96b7dcd832f124d48');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '23ad2e85f478b4d1e037');
