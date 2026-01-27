7c6cee90e6a161c473c4903e946a3bb37a1f17d4
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-01-27 11:33:59 +0600

    icon changed

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'f3a61f9365a5db4bdb6c');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '995d264369da74b3e956');
