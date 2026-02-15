1b50cd6d895087f22611d809c1eef561f7cfa170
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-02-15 15:56:08 +0600

    addons loading condition updated

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'aa76f1d13a42dd9e00b7');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '3b6fa5cf8b6a0d83d1b6');
