01eed4f583899d19c66567e85358f0c7de2d50e9
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-02-17 09:42:52 +0600

    setup page navigate to previous page from settings

--- assets/build/setup.1.0.0.asset.php
+++ assets/build/setup.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '1e91bcb533c77b8b65f9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '2893762cf2b623ba41ea');
