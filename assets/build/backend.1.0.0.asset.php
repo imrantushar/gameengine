b7078cb18d631cac335c0ea89cd260d907488247
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-03-10 02:19:45 +0600

    requirement component transition added

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '9cba02f9409b5ad366c4');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'ccc99efbaa92a3a4ab9c');
