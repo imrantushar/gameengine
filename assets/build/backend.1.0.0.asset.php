faa897b5914cf7d77babe7001fb5b898471f5181
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-04-13 15:37:22 +0600

    logger condition added

--- assets/build/backend.1.0.0.asset.php
+++ assets/build/backend.1.0.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '60c4b6d9c6be9d2ac063');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'ded7c093e80b52526303');
