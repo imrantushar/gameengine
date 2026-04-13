29a02efbe817dc136108153eae4e76f75d33c6ea
Kudratullah <mhamudul.hk@gmail.com>
2026-04-13 14:51:35 +0600

    Added SE-SDK for free version update & tracking

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'kodezen/gemcrm',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '44489b152fac7f83bbec7243592df8b4de2c99f8',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'kodezen/gemcrm' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '44489b152fac7f83bbec7243592df8b4de2c99f8',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'storeengine/wordpress-sdk' => array(
+            'pretty_version' => 'v1.3.0',
+            'version' => '1.3.0.0',
+            'reference' => '4fbdfe0d9580d7d8eaa742581c65873b93cbbde2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../storeengine/wordpress-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
