6aa010330b9dcd001cc972e3d3441dad4332c422
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-04-23 13:19:51 +0600

    solved tools tab issuesssss

--- assets/build/backend.1.1.0.asset.php
+++ assets/build/backend.1.1.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '5d493ccc6d37922efe5d');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '9895c2f96363a53df2c6');
