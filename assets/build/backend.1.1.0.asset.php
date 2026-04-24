62e015dfd0c951f483fd502ce1e66697fa2bff77
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-04-24 09:29:42 +0600

    soved shortcode item issues

--- assets/build/backend.1.1.0.asset.php
+++ assets/build/backend.1.1.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '85b47b002ab71b103e2b');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '9a47ab2482892838e98b');
--- assets/build/setup.1.1.0.asset.php
+++ assets/build/setup.1.1.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => 'fe3c8735da855e86e56d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '90e9591b56340202d920');
