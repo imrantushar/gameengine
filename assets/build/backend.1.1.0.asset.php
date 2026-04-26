01cddab30dcc469eda640305558273f61e8854d4
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-04-26 16:24:03 +0600

    Border and margin issue fixed

--- assets/build/backend.1.1.0.asset.php
+++ assets/build/backend.1.1.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '200bad1dec12a4365419');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'cc315d0fd6d2a9597083');
--- assets/build/setup.1.1.0.asset.php
+++ assets/build/setup.1.1.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '17bb8210366fdbebac15');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => 'a375d471a080f5c0c314');
