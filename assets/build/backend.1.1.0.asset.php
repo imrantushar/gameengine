78680ce125d1846f861d56b56b0b87b856ca85b7
mizanurrahman70 <mizanurrahmanofficial70@gmail.com>
2026-04-26 11:36:42 +0600

    solved adon button issues

--- assets/build/backend.1.1.0.asset.php
+++ assets/build/backend.1.1.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '99c38663914660ee6100');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'dd16003c3160ef97b4a8');
