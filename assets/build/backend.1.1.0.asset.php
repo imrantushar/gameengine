a45e98f05a6ebe0fd286f947dce7c67925662771
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-04-27 09:13:25 +0600

    settings tab select issue fixed

--- assets/build/backend.1.1.0.asset.php
+++ assets/build/backend.1.1.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '4141983720440ffc7468');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '89782718ed8a33e6783f');
