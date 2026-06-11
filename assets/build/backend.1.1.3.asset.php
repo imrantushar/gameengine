76ceffafc0eb35ca0d217ea9a529ba351d315d85
Tanjir Hasan <121637939+Tanjir-Hasan@users.noreply.github.com>
2026-06-11 10:31:30 +0600

    get help onClick updated

--- assets/build/backend.1.1.3.asset.php
+++ assets/build/backend.1.1.3.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'a3530647f1940cead195');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'b244d1e89faac9cd871e');
