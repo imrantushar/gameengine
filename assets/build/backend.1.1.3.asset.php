e97b33105eb4d17690fa669eff580d5f7fa5e076
Tanjir Hasan <121637939+Tanjir-Hasan@users.noreply.github.com>
2026-06-11 12:14:36 +0600

    settings sidebar icon update

--- assets/build/backend.1.1.3.asset.php
+++ assets/build/backend.1.1.3.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '002a62843c360cc117ba');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '7f6fd2320d480c46795b');
--- assets/build/setup.1.1.3.asset.php
+++ assets/build/setup.1.1.3.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => 'c0505f43f7cc8732f847');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => '439cdd8e4e9296fbc3ea');
