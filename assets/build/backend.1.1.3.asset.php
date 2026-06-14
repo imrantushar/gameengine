72f4c0df772fd8c4697f178166dfbb26810eb5c8
Tanjir Hasan <121637939+Tanjir-Hasan@users.noreply.github.com>
2026-06-14 09:55:14 +0600

    help modal width updated

--- assets/build/backend.1.1.3.asset.php
+++ assets/build/backend.1.1.3.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '1f7cf789ae94b471b271');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '88cd3790652641520b6c');
