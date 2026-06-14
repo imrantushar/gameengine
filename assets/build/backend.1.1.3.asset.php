4b380ac4260c33bceac6195441686b5d121e42a5
Tanjir Hasan <121637939+Tanjir-Hasan@users.noreply.github.com>
2026-06-14 09:50:56 +0600

    dashboard UI updated

--- assets/build/backend.1.1.3.asset.php
+++ assets/build/backend.1.1.3.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '4f540df5be5c57b58f42');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '1f7cf789ae94b471b271');
--- assets/build/setup.1.1.3.asset.php
+++ assets/build/setup.1.1.3.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => '9772418ba276026c135e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => '50f9d6dd6acdcff44dd5');
