9ad71d1e22f517e7fffd0f688b06e3271ede582b
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-09-06 11:30:46 +0600

    setup checkbox toggle issue fixed

--- assets/build/frontend.1.3.0.asset.php
+++ assets/build/frontend.1.3.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '41982b800b61cc32ad16');
+<?php return array('dependencies' => array(), 'version' => 'd26808a054adc68141fc');
--- assets/build/setup.1.3.0.asset.php
+++ assets/build/setup.1.3.0.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => '4b9d77f7d48317a8c223');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => '84ed4dbed89bcd5db252');
